380b89e0e8a3b766a405724c881dbe430b99f891
CNS-Kairos <167649647+CNS-Kairos@users.noreply.github.com>
2025-10-20 00:53:33 -0300

    Cambio completo a estilo.css
    Se creó un back-up del estilo.css que creé usando IA, porque el de la evaluación 1 era ridiculamente grande. Esta vez, decidí intentar tener una estructura de estilos modular, donde cada componente.
    
    Se modificó estilo.css para que tuviera clases en componentes de forma reutilizable, se creó un .css para el footer, home y nosotros. Se ajustó el import en index.js también.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
+// Estilos externos
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./styles/estilo.css";
-import "./styles/Navbar.css";
-//Este import es solo para los iconos de FontAwesome, por si acaso.
-import "@fortawesome/fontawesome-free/css/all.min.css"; 
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+// Estilos del proyecto - orden de carga importante
+import "./styles/estilo.css";        // Variables, base, banners, cards y formularios globales
+import "./styles/Navbar.css";        // Navbar
+import "./styles/Footer.css";        // Footer
+import "./styles/Home.css";          // Página Home
+import "./styles/Nosotros.css";      // Página Nosotros
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -20,3 +26,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
