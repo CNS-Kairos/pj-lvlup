79f5a025aaf34f4a05e85a7d6b10af790e970f00
CNS-Kairos <167649647+CNS-Kairos@users.noreply.github.com>
2025-10-25 20:43:12 -0300

    Estructura para ProductosAdmin
    Más importación de codigo desde paginas html antiguas.

--- src/admin/pages/ProductosAdmin.jsx
+++ src/admin/pages/ProductosAdmin.jsx
@@ -2,9 +2,36 @@
 // Página productos admin
 export default function ProductosAdmin() {
   return (
-    <div>
-      {/* Productos admin */}
-      <h2>Productos admin</h2>
-    </div>
+    <section>
+      <h2>Gestión de Productos</h2>
+      <p>Inventario actual de la tienda</p>
+
+      {/* Botón agregar producto */}
+      <div>
+        <button>
+          {/* Icono plus */}
+          Agregar Producto
+        </button>
+      </div>
+
+      {/* Tabla de productos */}
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Producto</th>
+              <th>Precio</th>
+              <th>Stock</th>
+              <th>Categoría</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* Los productos se cargarán aquí */}
+          </tbody>
+        </table>
+      </div>
+    </section>
   );
 }
