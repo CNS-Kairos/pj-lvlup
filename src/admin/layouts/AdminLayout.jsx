271a99aa92c118e370a1672d984d9aa54252bf49
CNS-Kairos <167649647+CNS-Kairos@users.noreply.github.com>
2025-10-27 18:50:48 -0300

    Ajustes en la página de AdminLayout
    Cambios en el enrutamiento y en la estructura de la pagina AdminLayout, ya que se intenta integrar la Sidebar donde está la información.

--- src/admin/layouts/AdminLayout.jsx
+++ src/admin/layouts/AdminLayout.jsx
@@ -1,31 +1,19 @@
 // AdminLayout para admin
 // Estructura base del panel admin
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import AdminSidebar from "../../components/AdminSidebar";
+import AdminFooter from "../../components/AdminFooter";
 
 export default function AdminLayout() {
   return (
-    <div>
+    <div className="admin-layout">
       {/* Sidebar admin */}
-      <nav>
-        <ul>
-          <li>
-            {/* Dashboard admin */}
-            <Link to="/admin">Dashboard admin</Link>
-          </li>
-          <li>
-            {/* Productos admin */}
-            <Link to="/admin/productos">Productos admin</Link>
-          </li>
-          <li>
-            {/* Usuarios admin */}
-            <Link to="/admin/usuarios">Usuarios admin</Link>
-          </li>
-        </ul>
-      </nav>
+      <AdminSidebar />
       {/* Contenido admin */}
-      <div>
+      <main className="admin-main">
         <Outlet />
-      </div>
+        <AdminFooter />
+      </main>
     </div>
   );
 }
