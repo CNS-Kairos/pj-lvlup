bc2da421a38d88484413c3b75dc79e2ebc1d33a5
CNS-Kairos <167649647+CNS-Kairos@users.noreply.github.com>
2025-10-28 22:51:52 -0300

    Modificación en publicLayout
    Se añade el componente <Notificacion />, esto es para que aparezca la notificación en todas las páginas públicas y flote sobre el contenido.

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -1,11 +1,12 @@
-// Layout para rutas públicas
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Notificacion from '../components/Notificacion';
 
 export default function PublicLayout() {
   return (
     <div className="d-flex flex-column min-vh-100">
+      <Notificacion />
       <Navbar />
       <main><Outlet /></main>
       <Footer />
