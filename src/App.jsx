e89462aefae0cfceed9d5470a7d92f3e3eb01bf3
CNS-Kairos <167649647+CNS-Kairos@users.noreply.github.com>
2025-10-29 01:12:57 -0300

    Se instala el AuthProvider en App.jsx
    Envuelve la aplicación con AuthProvider para poder mantener el estado global de autenticación para el resto de componentes.

--- src/App.jsx
+++ src/App.jsx
@@ -20,42 +20,48 @@ import NuevoProductoAdmin from "./admin/pages/NuevoProductoAdmin";
 import NuevoUsuarioAdmin from "./admin/pages/NuevoUsuarioAdmin";
 import { CarritoProvider } from "./hooks/useCarrito";
 import { NotificacionProvider } from "./context/NotificacionContext";
+import { AuthProvider } from "./context/AuthContext";
+import RutaAdmin from "./components/RutaAdmin";
 
 export default function App() {
   return (
-    <NotificacionProvider>
-      <CarritoProvider>
-        <BrowserRouter>
-          <Routes>
-            {/* Rutas públicas con su layout */}
-            <Route element={<PublicLayout />}>
-              <Route path="/" element={<Home />} />
-              <Route path="/nosotros" element={<Nosotros />} />
-              <Route path="/contacto" element={<Contacto />} />
-              <Route path="/blogs" element={<Blogs />} />
-              <Route
-                path="/blogs/nfts-en-videojuegos"
-                element={<DetalleBlog1 />}
-              />
-              <Route path="/blogs/5g-en-gaming" element={<DetalleBlog2 />} />
-              <Route path="/productos" element={<Productos />} />
-              <Route path="/producto/:id" element={<DetalleProducto />} />
-              <Route path="/carrito" element={<Carrito />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/registro" element={<Registro />} />
-              <Route path="*" element={<NotFound />} />
-            </Route>
-            {/* Rutas admin con su propio layout */}
-            <Route path="/admin" element={<AdminLayout />}>
-              <Route index element={<DashboardAdmin />} />
-              <Route path="productos" element={<ProductosAdmin />} />
-              <Route path="productos/nuevo" element={<NuevoProductoAdmin />} />
-              <Route path="usuarios" element={<UsuariosAdmin />} />
-              <Route path="usuarios/nuevo" element={<NuevoUsuarioAdmin />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </CarritoProvider>
-    </NotificacionProvider>
+    <AuthProvider>
+      <NotificacionProvider>
+        <CarritoProvider>
+          <BrowserRouter>
+            <Routes>
+              {/* Rutas públicas con su layout */}
+              <Route element={<PublicLayout />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/nosotros" element={<Nosotros />} />
+                <Route path="/contacto" element={<Contacto />} />
+                <Route path="/blogs" element={<Blogs />} />
+                <Route
+                  path="/blogs/nfts-en-videojuegos"
+                  element={<DetalleBlog1 />}
+                />
+                <Route path="/blogs/5g-en-gaming" element={<DetalleBlog2 />} />
+                <Route path="/productos" element={<Productos />} />
+                <Route path="/producto/:id" element={<DetalleProducto />} />
+                <Route path="/carrito" element={<Carrito />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/registro" element={<Registro />} />
+                <Route path="*" element={<NotFound />} />
+              </Route>
+              {/* Rutas admin con su propio layout */}
+              <Route path="/admin" element={<RutaAdmin />}>
+                <Route element={<AdminLayout />}>
+                  <Route index element={<DashboardAdmin />} />
+                  <Route path="productos" element={<ProductosAdmin />} />
+                  <Route path="productos/nuevo" element={<NuevoProductoAdmin />} />
+                  <Route path="usuarios" element={<UsuariosAdmin />} />
+                  <Route path="usuarios/nuevo" element={<NuevoUsuarioAdmin />} />
+                </Route>
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </CarritoProvider>
+      </NotificacionProvider>
+    </AuthProvider>
   );
 }
